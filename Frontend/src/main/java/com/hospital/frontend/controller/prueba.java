9ac7d16fe7689a547ed758d807910f1113941ee5
LHidalgo23 <luis.hidalgo.calvo@est.una.ac.cr>
2025-10-22 13:09:06 -0600

    Estructura del proyecto

---
+++ Frontend/src/main/java/com/hospital/frontend/controller/prueba.java
@@ -0,0 +1,7 @@
+package com.hospital.frontend.controller;
+
+public class prueba {
+    public static void main(String[] args) {
+        System.out.printf("Hola soy la prueba desde el frontend");
+    }
+}
